030a6d8a5e4f47ba9c3971adc4f3ea66a22d17b2
Thomas Harrison [sc22th] <sc22th@uol-pc-055401.leeds.ac.uk>
2025-02-03 14:18:27 +0000

    feat: Dijkstra's algorithm for directed & undirected graph and display results

---
+++ gralog-core/src/main/java/gralog/algorithm/Dijkstra.java
@@ -0,0 +1,95 @@
+package gralog.algorithm;
+
+import gralog.progresshandler.ProgressHandler;
+import gralog.structure.Edge;
+import gralog.structure.Structure;
+import gralog.structure.Vertex;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+
+
+//Djikstra's algorithm description matching gralog style - adds it to list of available algorithms
+@AlgorithmDescription(
+    name = "Dijkstra",
+    text = "Dijkstras algorithm for shortest path",
+    url = "https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm"
+)
+
+
+//Dijkstra class
+public class Dijkstra extends Algorithm{
+    
+    public static void dijkstra(Structure<Vertex, Edge> graph, Vertex start, HashMap<Vertex, Vertex> predecessor, HashMap<Vertex, Edge> edgeFromPredecessor, HashMap<Vertex, Double> distance, ProgressHandler progress){
+        //priority queue to store vertices ordered by distance from start vertex
+        PriorityQueue<Vertex> pQueue = new PriorityQueue<>(Comparator.comparingDouble(distance::get));
+        Boolean directed = false;
+
+        //set start vertex as zero distance and all other to infinity
+        distance.put(start, 0.0);
+        for(Vertex v : graph.getVertices()){
+            if (!v.equals(start)) {
+                distance.put(v, Double.POSITIVE_INFINITY);
+            }
+        }
+
+        //add start to priority queue
+        pQueue.add(start);
+
+        //continue through vertices in priority queue
+        while(!pQueue.isEmpty()){
+            Vertex u = pQueue.poll();
+
+            //get all edges incident to u
+            for (Edge edge : u.getIncidentEdges()){
+
+                //check if edge is dirceted
+                if (edge.isDirected){
+                    directed = true;
+                }
+                //get the other vertex of the edge. if undirected the target may be the same so get the source vertex instead
+                Vertex v = edge.getTarget();
+                if (directed == false && v.equals(u)){
+                    v = edge.getSource();
+                }
+
+                //calculate new distance
+                double dist = distance.get(u) + edge.weight;
+
+                //if new distance is less than current distance, update distance and add to priority queue
+                if (dist < distance.get(v)){
+                    distance.put(v, dist);
+                    predecessor.put(v, u);
+                    edgeFromPredecessor.put(v, edge);
+                    pQueue.add(v);
+                }
+            }
+        }
+    }
+
+    //run method
+    public Object run(Structure s, AlgorithmParameters p, Set<Object> selection, ProgressHandler onprogress) {
+        HashMap<Vertex, Vertex> predecessor = new HashMap<>();
+        HashMap<Vertex, Edge> edgeFromPredecessor = new HashMap<>();
+        HashMap<Vertex, Double> distance = new HashMap<>();        
+        Vertex v = selectedUniqueVertex(selection);
+
+        if (v == null)
+            return "Select one vertex to start Dijkstra's from.";
+
+        dijkstra(s, v, predecessor, edgeFromPredecessor, distance, onprogress);
+
+        //results
+        StringBuilder result = new StringBuilder();
+        result.append("Vertex\tDistance\n");
+        for (Vertex vertex : distance.keySet()) {
+            result.append(vertex.label).append("\t").append(distance.get(vertex)).append("\n");
+        }
+
+        return result.toString();
+    }
+}
